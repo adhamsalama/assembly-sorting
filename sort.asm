156a04e44d739ca21a22a1b6e1bcc3263c24e65e
Ahmed Moustafa <m1m0n@localhost.localdomain>
2021-01-03 01:23:39 +0200

    Add Data Segment

--- sort.asm
+++ sort.asm
@@ -1,1 +1,6 @@
 ; This program prompts user for array size, then sorts the array using selection sort algorithm and prints the sorted array.
+
+section .data
+    input_format: dq "%lld", 0
+    size_msg: dq "Enter array size : ", 0xA, 0
+    nl: db "", 0xA, 0
