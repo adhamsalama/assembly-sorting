3d33a09efa66824b7c1b05b70ba103d5391e30b7
Ahmed Younes <36298259+AhmedY0unes@users.noreply.github.com>
2021-01-05 23:28:56 +0200

    added the bss section

--- INSERTION_SORT.asm
+++ INSERTION_SORT.asm
@@ -10,3 +10,8 @@ section .data
     output_msg: dq "The sorted array is : ", 0xA, 0
     sorting_type_msg: dq "Choose 1 for Ascending sort or 2 for Decending : ", 0xA, 0 ; new
     new_line: db "", 0xA, 0
+    
+section .bss
+    size resq 2
+    array resq 21
+    sortingType resq 2          ;new
