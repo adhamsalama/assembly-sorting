a121dd7e09a13b9871212427324d826027f4cd8c
Ahmed Moustafa <m1m0n@localhost.localdomain>
2021-01-09 02:41:46 +0200

    Arrange The file

--- sort.asm
+++ sort.asm
@@ -39,6 +39,7 @@ INPUT_SIZE:
     ; Prompt user for input size
     mov RDI, size_msg
     call printf
+
     ; Scan input size
     mov RDI, input_format
     mov RSI, size
@@ -69,7 +70,7 @@ ARRAY_NUMBERS:
 INPUT_ARRAY: 						
     cmp RCX, [size]					; Check the size
     mov RDI, new_line
-    jz DONE				                ; Goto done after the input is complete
+    jz DONE				            ; Goto done after the input is complete
     mov [counter], RCX
     mov RAX, 0
     
@@ -127,64 +128,64 @@ SORT_TYPE:
  
     BS_ST_JUMP:    ;FOR OVERALL FILE bubble sort, sort type jump (A/D)
 
-  ; prompt user for sorting type
-    mov RDI, sorting_type_msg
-    call printf
-    mov RDI, input_format                   
-    mov RSI, sortingType
-    call scanf
-    
-    mov R15, [sortingType]                        ; R15 = sortingType = input
-    cmp R15, 1
-    je B_ASCENDING_SORTING                        ; if R15 (sorting type) == 1 [ASCENDING_SORTING]
-    cmp R15, 2
-    je B_DESCENDING_SORTING                       ; if R15 (sorting Type) == 2 [DESCENDING_SORTING]
-    
-  ; unexpected input   
-    mov RDI, invalid_input_msg
-    call printf 
-    mov RDI, new_line
-    call printf  
-    jmp BS_ST_JUMP                                ;reply the process of choosing bubbles sort type (A/D)
+        ; prompt user for sorting type
+          mov RDI, sorting_type_msg
+          call printf
+          mov RDI, input_format                   
+          mov RSI, sortingType
+          call scanf
+
+          mov R15, [sortingType]                        ; R15 = sortingType = input
+          cmp R15, 1
+          je B_ASCENDING_SORTING                        ; if R15 (sorting type) == 1 [ASCENDING_SORTING]
+          cmp R15, 2
+          je B_DESCENDING_SORTING                       ; if R15 (sorting Type) == 2 [DESCENDING_SORTING]
+
+        ; unexpected input   
+          mov RDI, invalid_input_msg
+          call printf 
+          mov RDI, new_line
+          call printf  
+          jmp BS_ST_JUMP                                ;reply the process of choosing bubbles sort type (A/D)
     
           ;***************** START ASCENDING BUBBLE SORT *********************
                                          
     B_ASCENDING_SORTING:         
-         mov RSI,[size]                                ; RSI = size
-         dec RSI                                       ; RSI = size -1
+         mov RSI,[size]                            ; RSI = size
+         dec RSI                                   ; RSI = size -1
 
     B_ASCENDING_SORTING_L1: 
 
 	     cmp RSI, 0
-	     je B_ASCENDING_SORTING_L1_END        ; if RSI = 0 (reach the start of the array) end sorting
-                                                  ; else sort
-         xor RDI, RDI                            ; RSI = 0
+	     je B_ASCENDING_SORTING_L1_END             ; if RSI = 0 (reach the start of the array) end sorting
+                                                   ; else sort
+         xor RDI, RDI                              ; RSI = 0
 
 	B_ASCENDING_SORTING_L2:
 
 		cmp RDI, RSI                    
-		jge B_ASCENDING_SORTING_L2_END              ; if RDI >= RSI (RDI reached the loop limit) stop sorting and repeat for RSI -= 1
-                                                  ; else start comparing
-		mov RAX, [array+RDI*8]            ; RAX = array[RDI]
-          inc RDI
-		mov RBX, [array+RDI*8]            ; RAX = array[RDI + 1]
-          dec RDI
+		jge B_ASCENDING_SORTING_L2_END             ; if RDI >= RSI (RDI reached the loop limit) stop sorting and repeat for RSI -= 1
+                                                   ; else start comparing
+		mov RAX, [array+RDI*8]                     ; RAX = array[RDI]
+        inc RDI
+		mov RBX, [array+RDI*8]                     ; RAX = array[RDI + 1]
+        dec RDI
 		cmp RAX, RBX                  
-		jg B_ASCENDING_SWAP                         ; if RAX > RBX swap them 
-                                                  ; else icrease RDI and repeat
+		jg B_ASCENDING_SWAP                        ; if RAX > RBX swap them 
+                                                   ; else icrease RDI and repeat
 		inc RDI
 		jmp B_ASCENDING_SORTING_L2
 
 	B_ASCENDING_SWAP:
 
-		mov [array+RDI*8], RBX            ; array[RDI] = RBX (array[RDI + 1])
-          inc RDI
-		mov [array+RDI*8], RAX            ; array[RDI + 1] = RAX (array[RDI])
+		mov [array+RDI*8], RBX                      ; array[RDI] = RBX (array[RDI + 1])
+        inc RDI
+		mov [array+RDI*8], RAX                      ; array[RDI + 1] = RAX (array[RDI])
 		jmp B_ASCENDING_SORTING_L2                  ; repeat with RDI += 1
 
 	B_ASCENDING_SORTING_L2_END:
 
-		dec RSI                           ; RSI -= 1
+		dec RSI                                     ; RSI -= 1
 		jmp B_ASCENDING_SORTING_L1                  ; start all over again
 
     B_ASCENDING_SORTING_L1_END:
@@ -193,63 +194,65 @@ SORT_TYPE:
 
     ;***************** START DESCENDING BUBBLE SORT *********************
    B_DESCENDING_SORTING: 
-    mov RSI,[size]
-    dec RSI
+        mov RSI,[size]
+        dec RSI
 
     B_DESCENDING_SORTING_L1: 
-	     cmp RSI, 0                           ; RSI = out, RDI = in, in JAVA code                           
-	     je B_DESCENDING_SORTING_L1_END       ; if RSI < 1 don't sort 
+	     cmp RSI, 0                               ; RSI = out, RDI = in, in JAVA code                           
+	     je B_DESCENDING_SORTING_L1_END           ; if RSI < 1 dont sort 
                                                   ; else sort them
-         mov RDI, 0                              ; RDI = 0
+         mov RDI, 0                               ; RDI = 0
 
 	B_DESCENDING_SORTING_L2:
 		cmp RDI, RSI
-		jge B_DESCENDING_SORTING_L2_END   ; if RDI >= RSI
-		mov RAX, [array+RDI*8]            ; RAX = array[RDI]
+		jge B_DESCENDING_SORTING_L2_END           ; if RDI >= RSI
+		mov RAX, [array+RDI*8]                    ; RAX = array[RDI]
         inc RDI
-		mov RBX, [array+RDI*8]            ; RBX = array[RDI+1]
+		mov RBX, [array+RDI*8]                    ; RBX = array[RDI+1]
         dec RDI
 		cmp RAX, RBX                  
-		jl B_DESCENDING_SWAP              ; if RAX < RBX swap them 
-		inc RDI                           ; else inc RDI and loop again
+		jl B_DESCENDING_SWAP                      ; if RAX < RBX swap them 
+		inc RDI                                   ; else inc RDI and loop again
 		jmp B_DESCENDING_SORTING_L2
 
 	B_DESCENDING_SWAP:
-		mov [array+RDI*8], RBX            ; array[RDI] = RBX
+		mov [array+RDI*8], RBX                    ; array[RDI] = RBX
         inc RDI
-		mov [array+RDI*8], RAX            ; array[RDI+1] = RAX
-		jmp B_DESCENDING_SORTING_L2       ; sorting L2
+		mov [array+RDI*8], RAX                    ; array[RDI+1] = RAX
+		jmp B_DESCENDING_SORTING_L2               ; sorting L2
 	B_DESCENDING_SORTING_L2_END:
-		dec RSI                           ; RSI -= 1
-		jmp B_DESCENDING_SORTING_L1       ; sorting again on the next element
+		dec RSI                                   ; RSI -= 1
+		jmp B_DESCENDING_SORTING_L1               ; sorting again on the next element
 
     B_DESCENDING_SORTING_L1_END:
-	mov RBX, 0
-    jmp PRINT_B_OUTPUT_MSG
+	    mov RBX, 0
+        jmp PRINT_B_OUTPUT_MSG
+
     ;***************** END DESCENDING BUBBLE SORT *********************
+
       ;scan input sorting type for insertion sort
 
-IS_ST_JUMP:        ;FOR OVERALL FILE insertion sort, sort type jump  (A/D)
+    IS_ST_JUMP:        ;FOR OVERALL FILE insertion sort, sort type jump  (A/D)
    
-   ; prompt user for sorting type
-    mov RDI, sorting_type_msg
-    call printf
-    mov RDI,input_format
-    mov RSI, sortingType
-    call scanf
-    mov R15, [sortingType]                    ; R15 = sortingType = input
-    
-    cmp R15, 1
-    je AISORT_INIT                               ; if R15 (sorting type) == 1 ASCENDING_SORTING
-    cmp R15, 2
-    je DISORT_INIT                              ; if R15 (sorting Type) == 2 DESCENDING_SORTING
-   
-   ; unexpected input 
-    mov RDI, invalid_input_msg
-    call printf
-    mov RDI, new_line
-    call printf    
-    jmp IS_ST_JUMP                      ;reply the process of choosing insertion sort type (A/D)
+        ; prompt user for sorting type
+         mov RDI, sorting_type_msg
+         call printf
+         mov RDI,input_format
+         mov RSI, sortingType
+         call scanf
+         mov R15, [sortingType]                    ; R15 = sortingType = input
+
+         cmp R15, 1
+         je AISORT_INIT                            ; if R15 (sorting type) == 1 ASCENDING_SORTING
+         cmp R15, 2
+         je DISORT_INIT                            ; if R15 (sorting Type) == 2 DESCENDING_SORTING
+
+        ; unexpected input 
+         mov RDI, invalid_input_msg
+         call printf
+         mov RDI, new_line
+         call printf    
+         jmp IS_ST_JUMP                            ;reply the process of choosing insertion sort type (A/D)
     
     ;***************** Ascending INSERTION SORT START ******************* 
  
@@ -258,31 +261,31 @@ IS_ST_JUMP:        ;FOR OVERALL FILE insertion sort, sort type jump  (A/D)
 AISORT_INIT:    ;initialize variable
     mov RSI, 0    
     
-AISORT_1:                                     ;outer loop
-	cmp RSI, [size]                       ;check if RSI >= size
-	jge AISORT1_END                       ;if it's true then jump to label AISORT1_END
-	mov RDI, RSI                          ;else continue and ignore the previous line RDI = RSI
-	inc RDI                               ;RDI++
-	AISORT_2:                             ;Inner Loop
-		cmp RDI, [size]               ;check if RDI >= size
-		jge AISORT2_END               ;if it's true exit the inner loop
-		mov RAX, [array+RSI*8]        ;RAX = array[RSI]
-		mov RBX, [array+RDI*8]        ;RBX = array[RDI]
-		cmp RAX, RBX                  ;check if RAX>RBX or array[i]>array[j]
-		jg AISWAP                     ;if true then jump to AISWAP lable to swap them
-		inc RDI                       ;else RDI++ and continue
-		jmp AISORT_2                  ;jump to the start of the inner loop
-	AISWAP:                               ;swap label
-		mov [array+RDI*8], RAX        ;array[j] = RAX
-		mov [array+RSI*8], RBX        ;array[i] = RBX
-		inc RDI                       ;RDI++
-		jmp AISORT_2                  ;return to the start of the inner loop
-	AISORT2_END:                          
-		inc RSI                       ;RSI++
-		jmp AISORT_1                  ;return to the start of the outer loop
+AISORT_1:                                          ;outer loop
+	cmp RSI, [size]                                ;check if RSI >= size
+	jge AISORT1_END                                ;if its true then jump to label AISORT1_END
+	mov RDI, RSI                                   ;else continue and ignore the previous line RDI = RSI
+	inc RDI                                        ;RDI++
+	AISORT_2:                                      ;Inner Loop
+		cmp RDI, [size]                            ;check if RDI >= size
+		jge AISORT2_END                            ;if its true exit the inner loop
+		mov RAX, [array+RSI*8]                     ;RAX = array[RSI]
+		mov RBX, [array+RDI*8]                     ;RBX = array[RDI]
+		cmp RAX, RBX                               ;check if RAX>RBX or array[i]>array[j]
+		jg AISWAP                                  ;if true then jump to AISWAP lable to swap them
+		inc RDI                                    ;else RDI++ and continue
+		jmp AISORT_2                               ;jump to the start of the inner loop
+	AISWAP:                                        ;swap label
+		mov [array+RDI*8], RAX                     ;array[j] = RAX
+		mov [array+RSI*8], RBX                     ;array[i] = RBX
+		inc RDI                                    ;RDI++
+		jmp AISORT_2                               ;return to the start of the inner loop
+	AISORT2_END:                                       
+		inc RSI                                    ;RSI++
+		jmp AISORT_1                               ;return to the start of the outer loop
 AISORT1_END:
-     mov RBX, 0                            ;RBX = 0
-     jmp PRINT_I_OUTPUT_MSG                ; jump to PRINT_OUTPUT_MSG label
+     mov RBX, 0                                    ;RBX = 0
+     jmp PRINT_I_OUTPUT_MSG                        ; jump to PRINT_OUTPUT_MSG label
 	
 ;***************** Ascending INSERTION SORT END *******************   
 
@@ -292,31 +295,31 @@ AISORT1_END:
   
 DISORT_INIT:    ;initialize variable
     mov RSI, 0                       
-DISORT_1:                                     ;outer loop
-	cmp RSI, [size]                       ;check if RSI >= size
-	jge DISORT1_END                       ;if it's true then jump to label AISORT1_END
-	mov RDI, RSI                          ;else continue and ignore the previous line RDI = RSI
-	inc RDI                               ;RDI++
-	DISORT_2:                             ;Inner Loop
-		cmp RDI, [size]               ;check if RDI >= size
-		jge DISORT2_END               ;if it's true exit the inner loop
-		mov RAX, [array+RSI*8]        ;RAX = array[RSI]
-		mov RBX, [array+RDI*8]        ;RBX = array[RDI]
-		cmp RAX, RBX                  ;check if RAX<RBX or array[i]>array[j]
-		jl DISWAP                     ;if true then jump to AISWAP lable to swap them
-		inc RDI                       ;else RDI++ and continue
-		jmp DISORT_2                  ;jump to the start of the inner loop
-	DISWAP:                               ;swap label
-		mov [array+RDI*8], RAX        ;array[j] = RAX
-		mov [array+RSI*8], RBX        ;array[i] = RBX
-		inc RDI                       ;RDI++
-		jmp DISORT_2                  ;return to the start of the inner loop
+DISORT_1:                                          ;outer loop
+	cmp RSI, [size]                                ;check if RSI >= size
+	jge DISORT1_END                                ;if its true then jump to label AISORT1_END
+	mov RDI, RSI                                   ;else continue and ignore the previous line RDI = RSI
+	inc RDI                                        ;RDI++
+	DISORT_2:                                      ;Inner Loop
+		cmp RDI, [size]                            ;check if RDI >= size
+		jge DISORT2_END                            ;if its true exit the inner loop
+		mov RAX, [array+RSI*8]                     ;RAX = array[RSI]
+		mov RBX, [array+RDI*8]                     ;RBX = array[RDI]
+		cmp RAX, RBX                               ;check if RAX<RBX or array[i]>array[j]
+		jl DISWAP                                  ;if true then jump to AISWAP lable to swap them
+		inc RDI                                    ;else RDI++ and continue
+		jmp DISORT_2                               ;jump to the start of the inner loop
+	DISWAP:                                        ;swap label
+		mov [array+RDI*8], RAX                     ;array[j] = RAX
+		mov [array+RSI*8], RBX                     ;array[i] = RBX
+		inc RDI                                    ;RDI++
+		jmp DISORT_2                               ;return to the start of the inner loop
 	DISORT2_END:
-		inc RSI                       ;RSI++
-		jmp DISORT_1                  ;return to the start of the outer loop
+		inc RSI                                    ;RSI++
+		jmp DISORT_1                               ;return to the start of the outer loop
 DISORT1_END:
-    mov RBX, 0                            ;RBX = 0
-    jmp PRINT_I_OUTPUT_MSG                ; jump to PRINT_OUTPUT_MSG label
+    mov RBX, 0                                     ;RBX = 0
+    jmp PRINT_I_OUTPUT_MSG                         ; jump to PRINT_OUTPUT_MSG label
         
         
 ;***************** Decending INSERTION SORT END *******************
@@ -330,21 +333,21 @@ SS_ST_JUMP:
 
        ; Scan sorting type
         mov RDI, input_format
-        mov RSI, sortingType                  ; R15 = sortingType = input
+        mov RSI, sortingType                       ; R15 = sortingType = input
         call scanf
         mov R15, [sortingType]
         
         cmp R15, 1
-        je S_ASCENDING_OUTTER_LOOP            ; if R15 (sorting type) == 1 ASCENDING_SORTING
+        je S_ASCENDING_OUTTER_LOOP                 ; if R15 (sorting type) == 1 ASCENDING_SORTING
         cmp R15, 2
-        je S_DESCENDING_OUTTER_LOOP           ; if R15 (sorting type) == 1 DESCENDING_SORTING
+        je S_DESCENDING_OUTTER_LOOP                ; if R15 (sorting type) == 1 DESCENDING_SORTING
         
       ; unexpected input     
         mov RDI, invalid_input_msg
         call printf 
         mov RDI, new_line
         call printf  
-        jmp SS_ST_JUMP                     ;reply the process of choosing selection sort type (A/D)
+        jmp SS_ST_JUMP                             ;reply the process of choosing selection sort type (A/D)
 	      
 	      ;***************** START ASCENDING SELCTION SORT *********************
 
@@ -465,18 +468,18 @@ PRINT_I_OUTPUT_MSG:
         jmp PRINT_ARRAY
  
  PRINT_ARRAY:						
-       ;Print array
-       ; Iterate through the array and print each element and then goto END when reaching size
+    ;Print array
+    ; Iterate through the array and print each element and then goto END when reaching size
 
 	cmp RCX, [size]
 	jz END
 
-       ;Get the next element at the array and put it in RAX then increment the counter
+    ;Get the next element at the array and put it in RAX then increment the counter
 	mov RAX, [array+RCX*8]			
 	inc RCX	
 	mov [counter], RCX
 
-       ;Print the current element
+    ;Print the current element
 	mov RDI, output_format
 	mov RSI, RAX
 	call printf
@@ -485,13 +488,13 @@ PRINT_I_OUTPUT_MSG:
 	jmp PRINT_ARRAY
 
  END:	
-       ; Print newline and clear registers
+    ; Print newline and clear registers
 	mov RDI, new_line
 	call printf
 
 	xor RAX, RAX; RAX = 0
-        xor RCX, RCX; RCX = 0
-        xor RBX, RBX; RBX = 0
+    xor RCX, RCX; RCX = 0
+    xor RBX, RBX; RBX = 0
 	
 	pop RBP
 	ret
